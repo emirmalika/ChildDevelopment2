688789212a8c0b269d51e48fbcee60e4f42b0858
emirmalika <emirmalika0712@gmail.com>
2022-12-30 19:32:14 +0600

    Initial Commit

---
+++ ChildDevelopment2/ChildDevelopment2App.swift
@@ -0,0 +1,17 @@
+//
+//  ChildDevelopment2App.swift
+//  ChildDevelopment2
+//
+//  Created by Malik Em on 30.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChildDevelopment2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
